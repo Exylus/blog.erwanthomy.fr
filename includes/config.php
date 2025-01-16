681382a9f7790379c7f34991fc98913f8a5e8d81
Erwan Thomy <thomy.erwan@gmail.com>
2025-01-16 13:15:34 +0100

    Added basic navbar

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+// config.php
+$host = 'localhost';
+$db   = 'blog.erwanthomy.fr';
+$user = 'erwan';
+$pass = 'ghYW5hpRY6n';
+$charset = 'utf8mb4';
---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<div id="home">
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Dashboard</a></li>
+            <?php
+            // TODO - Change True to Logged in 
+            echo (True) ? '<li><a href="#">Sign Out</a></li>' : '(<li><a href="#">Sign In</a></li>)';
+            ?>
+        </ul>
+    </nav>
+</div>
