d49ebd9e46a794b4982648611489a156f0d735f2
Erwan Thomy <thomy.erwan@gmail.com>
2025-01-16 19:39:52 +0100

    Added navbar

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@
             <li><a href="#">Dashboard</a></li>
             <?php
             // TODO - Change True to Logged in 
-            echo (True) ? '<li><a href="#">Sign Out</a></li>' : '(<li><a href="#">Sign In</a></li>)';
+            echo (True) ? '<li><a href="#">Sign Out</a></li>' : '<li><a href="#">Sign In</a></li>';
             ?>
         </ul>
     </nav>
