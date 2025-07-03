c11f1bb34d59fef5b2cfa0c8863c530f29359bd8
Ny Aina Rabarisoa <nekenarab55@gmail.com>
2025-07-03 12:08:42 +0200

    Registration form management

--- post.php
+++ post.php
@@ -5,7 +5,11 @@ include "includes/header.php";
 
 <div class="container py-5">
     <h1 class="text-black">Welcome</h1>
-    <button onclick="location.href='create_post.php'" class="btn btn-success">+</button>
+    <p class="mt-5">
+        <button onclick="location.href='create_post.php'" class="btn btn-dark">+</button>
+        <h2>Post something</h2>
+    </p>
+    
 </div>
 <div>
     <output id="postArea"></output>
--- register.php
+++ register.php
@@ -1,3 +1,16 @@
+<?php
+if (
+    !empty($_POST['name']) && !empty($_POST['dob']) && !empty($_POST['username']) && !empty($_POST['password']) && !empty($_POST['repeat_password']) && isset($_POST['checkDefault'])
+) {
+    if ($_POST['password'] === $_POST['repeat_password']) {
+        header("Location: post.php");
+        exit();
+    } else {
+        $error = "Passwords do not match.";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,50 +28,58 @@
     <div class="container d-flex justify-content-center">
         <div class="shadow p-4 mb-5 mt-5 bg-body-tertiary rounded" style="width: 600px;">
             <h2 class="text-center">Create account</h2>
-            <form enctype="multipart/form-data">
+            <form method="post" action="" enctype="multipart/form-data">
                 <div class="row mt-4">
                     <div class="col-7 mb-3">
                         <label class="text-black">Name</label>
-                        <input type="text" class="text-black form-control mt-2" id="name" required>
+                        <input type="text" name="name" class="text-black form-control mt-2" required>
                     </div>
                     <div class="col-5 mb-3">
                         <label for="dob" class="form-label text-black">Date of birth</label>
                         <div class="input-group">
-                            <input type="date" class="form-control" id="dob" name="dob" required>
+                            <input type="date" class="form-control" name="dob" required>
                         </div>
                     </div>
                 </div>
 
                 <!--Add phone number -->
-                
+
 
                 <div class="mb-3">
                     <label class="text-black">Username</label>
-                    <input type="text" class="text-black form-control mt-1" id="usrname" required>
+                    <input type="text" class="text-black form-control mt-1" name="username" required>
                 </div>
                 <div class="mb-4">
                     <label class="text-black">Password</label>
-                    <input type="password" class="text-black form-control mt-1" id="psswd" required>
+                    <input type="password" class="text-black form-control mt-1" name="password" required>
 
                     <!-- À ajouter : hide/show password -->
 
                 </div>
                 <div class="mb-4">
                     <label class="text-black">Repeat password</label>
-                    <input type="password" class="text-black form-control mt-1" id="repeaPsswd" required>
+                    <input type="password" class="text-black form-control mt-1" name="repeat_password" required>
 
                     <!-- À ajouter : hide/show password -->
 
                 </div>
-                
+
                 <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="checkDefault">
+                    <input class="form-check-input" type="checkbox" value="" name="checkDefault" required>
                     <label class="form-check-label" for="checkDefault">
                         I agree agree all statements in <a class="link" href="#">Terms of service</a>
                     </label>
                 </div>
+                
+                <!-- Error message -->
+                <?php if (!empty($error)): ?>
+                    <div class="mt-3 mb-3 p-2 text-danger text-center">
+                        <?= htmlspecialchars($error) ?>
+                    </div>
+                <?php endif; ?>
+
                 <div class="text-center">
-                    <button type="submit" onclick="location.href='post.php'" class="btn btn-primary mt-4">
+                    <button type="submit" class="btn btn-primary mt-4">
                         Register
                     </button>
                     <p class="mt-5"><a class="link" href="login.php">I already have an account</a></p>
@@ -68,6 +89,7 @@
     </div>
 </body>
 <div>
-    
+
 </div>
+
 </html>
